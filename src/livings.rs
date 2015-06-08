e8aa30aeb848208f28c30f8d2fcc6170dd1ca729
Guillaume Gomez <guillaume1.gomez@gmail.com>
2015-06-08 10:00:37 +0200

    Update livings.rs

--- src/livings.rs
+++ src/livings.rs
@@ -2,7 +2,9 @@ create_something!(Cat);
 create_something!(Dog);
 create_something!(Human);
 create_something!(Goat);
+create_something!(michaelsproul);
 
 create_entity!(God);
 create_entity!(Satan);
 create_entity!(Goat666);
+create_entity!(Manisheart);
